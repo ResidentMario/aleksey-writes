59615ed96535061dbce899652349dfce994c7083
Aleksey Bilogur <aleksey@residentmar.io>
2021-01-24 18:05:22 -0500

    Init app part.

---
+++ js/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import SearchBox from './search_box';
+
+// import rp from 'request-promise-native';
+
+
+class App extends Component {
+    render() {
+        return <SearchBox />;
+    }
+
+}
+
+export default App;
---
+++ js/components/search_box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+class SearchBox extends Component {
+    render() {
+        return <div className="search-box-frame">
+            HELLO
+        </div>
+    }
+}
+
+export default SearchBox;
